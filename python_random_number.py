4e779f90f108f78b69362cf9729277e54344816f
programerAta <nafasama379@gmail.com>
2025-10-17 18:04:08 +0330

    test_object change name of vars

--- python_random_number.py
+++ python_random_number.py
@@ -13,24 +13,24 @@ if tanzimat=="normal":
         elif Number>Number_random:
             print(f"The Number Is lower Than {Number}")
 elif tanzimat=="heart":
-    meghdar_jan=int(input("enter your amount of heart:\n"))
-    jan=meghdar_jan
+    meghdar_hp=int(input("enter your amount of heart:\n"))
+    hp=meghdar_hp
     Number_vasat=list( map(int,input("select two number \n").split()))
     Number_random=random.randint(min(Number_vasat),max (Number_vasat))
     while True:
         Number=int(input("Enter Number: \n"))
         if Number==Number_random:
             print("You Win")
-            jan=meghdar_jan
+            hp=meghdar_hp
             Number_random=random.randint(min(Number_vasat),max (Number_vasat))
         elif Number<Number_random:
             print(f"The Number Is More Than {Number}")
-            jan-=1
+            hp-=1
         elif Number>Number_random:
             print(f"The Number Is lower Than {Number}")
-            jan-=1
-        if jan==0:
-            jan=meghdar_jan
+            hp-=1
+        if hp==0:
+            hp=meghdar_hp
             print("you lose")
             print(f"The Number Was {Number_random}")
             Number_random=random.randint(min(Number_vasat),max (Number_vasat))
