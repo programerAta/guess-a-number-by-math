ede93dfcbda0ad958c583712be1189dc3cdf1c7b
programerAta <nafasama379@gmail.com>
2025-10-17 17:58:06 +0330

    tes_object update some name

--- python_random_number.py
+++ python_random_number.py
@@ -1,36 +1,36 @@
 import random
 tanzimat=input("heart or normal\n")
 if tanzimat=="normal":
-    adad_vasat=list( map(int,input("select two number").split()))
-    adad_random=random.randint(min(adad_vasat),max (adad_vasat))
+    Number_vasat=list( map(int,input("select two number").split()))
+    Number_random=random.randint(min(Number_vasat),max (Number_vasat))
     while True:
-        adad=int(input("Enter Number: \n"))
-        if adad==adad_random:
+        Number=int(input("Enter Number: \n"))
+        if Number==Number_random:
             print("You Win")
-            adad_random=random.randint(min(adad_vasat),max (adad_vasat))
-        elif adad<adad_random:
-            print(f"The Number Is More Than {adad}")
-        elif adad>adad_random:
-            print(f"The Number Is lower Than {adad}")
+            Number_random=random.randint(min(Number_vasat),max (Number_vasat))
+        elif Number<Number_random:
+            print(f"The Number Is More Than {Number}")
+        elif Number>Number_random:
+            print(f"The Number Is lower Than {Number}")
 elif tanzimat=="heart":
     meghdar_jan=int(input("enter your amount of heart:\n"))
     jan=meghdar_jan
-    adad_vasat=list( map(int,input("select two number \n").split()))
-    adad_random=random.randint(min(adad_vasat),max (adad_vasat))
+    Number_vasat=list( map(int,input("select two number \n").split()))
+    Number_random=random.randint(min(Number_vasat),max (Number_vasat))
     while True:
-        adad=int(input("Enter Number: \n"))
-        if adad==adad_random:
+        Number=int(input("Enter Number: \n"))
+        if Number==Number_random:
             print("You Win")
             jan=meghdar_jan
-            adad_random=random.randint(min(adad_vasat),max (adad_vasat))
-        elif adad<adad_random:
-            print(f"The Number Is More Than {adad}")
+            Number_random=random.randint(min(Number_vasat),max (Number_vasat))
+        elif Number<Number_random:
+            print(f"The Number Is More Than {Number}")
             jan-=1
-        elif adad>adad_random:
-            print(f"The Number Is lower Than {adad}")
+        elif Number>Number_random:
+            print(f"The Number Is lower Than {Number}")
             jan-=1
         if jan==0:
             jan=meghdar_jan
             print("you lose")
-            print(f"The Number Was {adad_random}")
-            adad_random=random.randint(min(adad_vasat),max (adad_vasat))
+            print(f"The Number Was {Number_random}")
+            Number_random=random.randint(min(Number_vasat),max (Number_vasat))
